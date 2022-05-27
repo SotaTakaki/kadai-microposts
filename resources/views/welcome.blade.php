fc9eed49be33cefa6185ab4939ac1393bd7b7da1
Sota Takaki <sota-takaki@dac.co.jp>
2022-05-27 06:47:32 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,16 @@
 @extends("layouts.app")
 
 @section("content")
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
             
-            {{--　ユーザー登録リンク --}}
-            {!! link_to_route("signup.get", "Sign up now!", [], ["class" => "btn btn-lg btn-primary"]) !!}
+                {{--　ユーザー登録リンク --}}
+                {!! link_to_route("signup.get", "Sign up now!", [], ["class" => "btn btn-lg btn-primary"]) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
